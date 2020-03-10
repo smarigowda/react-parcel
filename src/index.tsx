9e1808a5310f63f84e63ba9ea611584a38692afc
Santosh Marigowda <smarigowda@ruffer.co.uk>
2020-03-10 15:32:29 +0000

    added SignalR client

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import { Component } from "react";
 import { render } from "react-dom";
 import "./index.css";
 import Count from "./components/Count";
+import * as signalR from "@microsoft/signalr";
 
+
+// setup and initiate SignalR
+let connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chat")
+    .build();
+    
 class App extends Component {
   render() {
     return (
