1d6dcff81f6e5df6a2e27974fa3c0f4ed05e4519
Santosh Marigowda <smarigowda@ruffer.co.uk>
2020-03-10 16:16:07 +0000

    using functional component

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,30 @@
 import * as React from "react";
-import { Component } from "react";
 import { render } from "react-dom";
+import * as signalR from "@microsoft/signalr";
+// local
 import "./index.css";
 import Count from "./components/Count";
-import * as signalR from "@microsoft/signalr";
 
-// setup and initiate SignalR
+// SignalR
 let connection = new signalR.HubConnectionBuilder()
   .withUrl("https://localhost:5001/chatHub")
   .build();
 
 connection.on("ReceiveMessage", data => {
-  console.log(`A message received from server (method = ReceiveMessage) ${data}`);
+  console.log(
+    `A message received from server (method = ReceiveMessage) ${data}`
+  );
 });
 
 connection.start().then(() => connection.invoke("SendMessage", "Hello"));
-class App extends Component {
-  render() {
-    return (
-      <aside>
-        <h1>Hello World!</h1>
-        <Count count={100}></Count>
-      </aside>
-    );
-  }
-}
+
+const App = () => {
+  return (
+    <aside>
+      <h1>Hello SignalR !</h1>
+      {/* <Count count={0}></Count> */}
+    </aside>
+  );
+};
 
 render(<App />, document.getElementById("root"));
